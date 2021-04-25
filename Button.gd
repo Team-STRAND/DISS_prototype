199dbed2ee7abd0d61aa880ec117e6379eaccd50
cristi <thiess.cristi@gmail.com>
2021-04-25 09:33:03 +0300

    shape scene

---
+++ Button.gd
@@ -0,0 +1,14 @@
+extends TextureButton
+
+onready var parent = get_parent()
+var move=false;
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	if(move):
+		parent.set_offset(parent.get_offset() + 300 * delta)
+
+func _on_Button_pressed():
+	move=true
---
+++ WrongShape.gd
@@ -0,0 +1,9 @@
+extends TextureButton
+
+
+func _ready():
+	get_child(0).visible=false
+	
+
+func _on_Button_pressed():
+	get_child(0).visible=true
